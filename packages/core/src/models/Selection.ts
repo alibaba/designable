973d69eb87b58a416d5310d249085a93c5e41b63
janryWang <zhili.wzl@alibaba-inc.com>
2021-06-11 21:20:52 +0800

    refactor(core): improve selection performance

--- packages/core/src/models/Selection.ts
+++ packages/core/src/models/Selection.ts
@@ -46,6 +46,7 @@ export class Selection {
 
   select(id: string | TreeNode) {
     if (isStr(id)) {
+      if (this.selected.includes(id)) return
       this.selected = [id]
       this.trigger()
     } else {
