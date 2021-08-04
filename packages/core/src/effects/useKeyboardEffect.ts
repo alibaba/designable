8263c49a2502b20817f989cfc0d018541d261c11
zhili.wzl <zhili.wzl@alibaba-inc.com>
2021-08-04 15:59:44 +0800

    fix(core): fix shortcut can not get context

--- packages/core/src/effects/useKeyboardEffect.ts
+++ packages/core/src/effects/useKeyboardEffect.ts
@@ -5,20 +5,16 @@ export const useKeyboardEffect = (engine: Engine) => {
   engine.subscribeTo(KeyDownEvent, (event) => {
     const keyboard = engine.keyboard
     if (!keyboard) return
-    keyboard.handleKeyboard(event, {
-      ...event.context,
-      workspace:
-        engine.workbench.activeWorkspace || engine.workbench.currentWorkspace,
-    })
+    const workspace =
+      engine.workbench.activeWorkspace || engine.workbench.currentWorkspace
+    keyboard.handleKeyboard(event, workspace.getEventContext())
   })
 
   engine.subscribeTo(KeyUpEvent, (event) => {
     const keyboard = engine.keyboard
     if (!keyboard) return
-    keyboard.handleKeyboard(event, {
-      ...event.context,
-      workspace:
-        engine.workbench.activeWorkspace || engine.workbench.currentWorkspace,
-    })
+    const workspace =
+      engine.workbench.activeWorkspace || engine.workbench.currentWorkspace
+    keyboard.handleKeyboard(event, workspace.getEventContext())
   })
 }
