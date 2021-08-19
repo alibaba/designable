7aebb2e4701cd6229b53b1114d47ad81e3257774
zhili.wzl <zhili.wzl@alibaba-inc.com>
2021-08-19 13:48:12 +0800

    fix(settings-form): fix ui can not reactive with node.props changed

--- packages/react-settings-form/src/SettingsForm.tsx
+++ packages/react-settings-form/src/SettingsForm.tsx
@@ -38,7 +38,7 @@ export const SettingsForm: React.FC<ISettingFormProps> = observer(
           useSnapshot(operation)
         },
       })
-    }, [node, schema, operation])
+    }, [node, node.props, schema, operation])
 
     const isEmpty = !(
       node &&
