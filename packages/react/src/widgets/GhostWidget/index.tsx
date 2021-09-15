f46e80ff91749a8abf77665b97c168b4acd6d9a0
zhili.wzl <zhili.wzl@alibaba-inc.com>
2021-09-15 16:30:14 +0800

    perf(react): imporve ghost move performance

--- packages/react/src/widgets/GhostWidget/index.tsx
+++ packages/react/src/widgets/GhostWidget/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { useCursor, usePrefix, useDesigner } from '../../hooks'
 import { CursorStatus } from '@designable/core'
+import { autorun } from '@formily/reactive'
 import { observer } from '@formily/reactive-react'
 import { NodeTitleWidget } from '../NodeTitleWidget'
 import './styles.less'
@@ -8,9 +9,21 @@ import './styles.less'
 export const GhostWidget = observer(() => {
   const designer = useDesigner()
   const cursor = useCursor()
+  const ref = useRef<HTMLDivElement>()
   const prefix = usePrefix('ghost')
   const draggingNodes = designer.findDraggingNodes()
   const firstNode = draggingNodes[0]
+  useEffect(
+    () =>
+      autorun(() => {
+        const transform = `perspective(1px) translate3d(${
+          cursor.position?.topClientX - 18
+        }px,${cursor.position?.topClientY - 12}px,0) scale(0.8)`
+        if (!ref.current) return
+        ref.current.style.transform = transform
+      }),
+    []
+  )
   const renderNodes = () => {
     return (
       <span
@@ -25,14 +38,7 @@ export const GhostWidget = observer(() => {
   }
   if (!firstNode) return null
   return cursor.status === CursorStatus.Dragging ? (
-    <div
-      className={prefix}
-      style={{
-        transform: `perspective(1px) translate3d(${
-          cursor.position?.topClientX - 18
-        }px,${cursor.position?.topClientY - 12}px,0) scale(0.8)`,
-      }}
-    >
+    <div ref={ref} className={prefix}>
       {renderNodes()}
     </div>
   ) : null
