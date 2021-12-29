0161df22b78d23bf9282532fbe4044c62db09094
jinphic <13719174004@163.com>
2021-12-30 00:25:08 +0800

    fix(antd): revert unnecessary feature

--- formily/antd/src/components/Card/preview.tsx
+++ formily/antd/src/components/Card/preview.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card as AntdCard } from 'antd'
-import { SourceIcon } from './sourceIcon'
+
 import { createBehavior, createResource } from '@designable/core'
 import { DnFC } from '@designable/react'
 import { createVoidFieldSchema } from '../Field'
@@ -29,70 +29,12 @@ Card.Behavior = createBehavior({
   designerProps: {
     droppable: true,
     propsSchema: createVoidFieldSchema(AllSchemas.Card),
-    resizable: {
-      width(node, element) {
-        const width = Number(
-          node.props['x-component-props']?.style?.width ??
-            element.getBoundingClientRect().width
-        )
-        return {
-          plus: () => {
-            node.props['x-component-props'] =
-              node.props['x-component-props'] || {}
-            node.props['x-component-props'].style =
-              node.props['x-component-props'].style || {}
-            node.props['x-component-props'].style.width = width + 6
-          },
-          minus: () => {
-            node.props['x-component-props'] =
-              node.props['x-component-props'] || {}
-            node.props['x-component-props'].style =
-              node.props['x-component-props'].style || {}
-            node.props['x-component-props'].style.width = width - 6
-          },
-        }
-      },
-    },
-    translatable: {
-      x(node, element, diffX) {
-        const left =
-          parseInt(
-            node.props['x-component-props']?.style?.left ?? element?.style.left
-          ) || 0
-        return {
-          translate: () => {
-            node.props['x-component-props'] =
-              node.props['x-component-props'] || {}
-            node.props['x-component-props'].style =
-              node.props['x-component-props'].style || {}
-            node.props['x-component-props'].style.left =
-              left + parseInt(String(diffX)) + 'px'
-          },
-        }
-      },
-      y(node, element, diffY) {
-        const top =
-          parseInt(
-            node.props['x-component-props']?.style?.top ?? element?.style.top
-          ) || 0
-        return {
-          translate: () => {
-            node.props['x-component-props'] =
-              node.props['x-component-props'] || {}
-            node.props['x-component-props'].style =
-              node.props['x-component-props'].style || {}
-            node.props['x-component-props'].style.top =
-              top + parseInt(String(diffY)) + 'px'
-          },
-        }
-      },
-    },
   },
   designerLocales: AllLocales.Card,
 })
 
 Card.Resource = createResource({
-  icon: <SourceIcon />,
+  icon: 'CardSource',
   elements: [
     {
       componentName: 'Field',
